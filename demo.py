deed3f174a3e356adfc6244c0654a7c4b0b9efa2
AndrewK225 <andrew.koe315@gmail.com>
2016-05-17 01:38:41 +0000

    pi compatible

--- demo.py
+++ demo.py
@@ -1,5 +1,7 @@
 import knn
 import naiveBayes
+import sys
+import os
 
 trainingSet,lexicon = knn.train()
 naiveBayes.trainNaiveBayes("train_formatted.txt")
@@ -23,17 +25,25 @@ while(1):
             pass
     
     if pos > neg:
-        print("Baseline: positive")
+       pass
+       #print("Baseline: positive")
     elif pos < neg:
-        print("Baseline: negative")
+        pass
+        #print("Baseline: negative")
     else:
-        print("Baseline: neutral")
-    
+        pass
+        #print("Baseline: neutral")
+   
     #Naive Bayes 
     print "Naive Bayes: "+ naiveBayes.makePrediction(test_in)
-
+    if naiveBayes.makePrediction(test_in) == "positive":
+        os.system("sudo ../rpi-rgb-led-matrix/led-matrix -r 16 -t 2 -D 0")
+    elif naiveBayes.makePrediction(test_in) == "neutral":
+        os.system("sudo ../rpi-rgb-led-matrix/led-matrix -r 16 -t 2 -D 5")
+    else:
+	os.system("sudo ../rpi-rgb-led-matrix/led-matrix -r 16 -t 2 -D 4")
     #KNN
-    knn_test_feats = knn.processTest(lexicon,test_in)
-    knn_pred = knn.test(trainingSet, knn_test_feats, k)
-    print("KNN (k=5): " + knn_pred)
+ #   knn_test_feats = knn.processTest(lexicon,test_in)
+ #   knn_pred = knn.test(trainingSet, knn_test_feats, k)
+ #   print("KNN (k=5): " + knn_pred)
 
